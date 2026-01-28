e88f538463574707febdf1ce3066e12e89da25f5
Ajay Vijay <ajayvijay7723@gmail.com>
2026-01-28 14:35:23 +0530

    Playwright tests + CI workflow

--- playwright-m9/tests/Click “Forgot your password.spec.js
+++ playwright-m9/tests/Click “Forgot your password.spec.js
@@ -1,6 +1,8 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-  await page.goto('https://wepixstudio.app/login');
+  await page.goto('https://wepixstudio.app/');
+  await page.getByRole('button', { name: 'Log in' }).click();
   await page.getByRole('link', { name: 'Forgot your password?' }).click();
+  await page.getByRole('heading', { name: 'Forgot password?' }).click();
 });
